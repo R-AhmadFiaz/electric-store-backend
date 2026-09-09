4562c0e862740873d35696bbe5baebfbfe220ba1
AHMAD CLOUD <ahmadcloudarc@gmail.com>
2026-09-09 02:23:13 -0700

    fix app.ts error

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,8 +1,8 @@
-import  express  from 'express'
+import { Router } from 'express'
 import {createCategory} from '../controllers/category.controller.js'
 import { verifyJWT } from '../middleware/auth.middleware.js'
 
-export const categoryRouter = express()
+export const categoryRouter = Router()
 
 categoryRouter.route('/create-category').post(verifyJWT, createCategory)
 
