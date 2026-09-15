13e93a7d143e5a674475b2308fc52eedc135b608
AHMAD CLOUD <ahmadcloudarc@gmail.com>
2026-09-15 09:50:36 -0700

    create routes for all orders

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,13 +2,16 @@
 import { Router } from "express";
 
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { cancelOrder, createOrder, updateOrderStatus } from "../controllers/order.controller.js";
+import { cancelOrder, createOrder, createQuotation, updateOrderStatus, updateQuotationStatus, viewStockLog } from "../controllers/order.controller.js";
 
 export const orderRouter = Router()
 
-orderRouter.route('/create-order').post(verifyJWT, createOrder)
-orderRouter.route('/:orderId/cancel-order').patch(verifyJWT, cancelOrder)
-orderRouter.route('/:orderId/update-order-status').patch(verifyJWT, updateOrderStatus)
+orderRouter.route('/').post(verifyJWT, createOrder)
+orderRouter.route('/:orderId/cancel').patch(verifyJWT, cancelOrder)
+orderRouter.route('/:orderId/status').patch(verifyJWT, updateOrderStatus)
+orderRouter.route('/:orderId/quotation').post(verifyJWT, createQuotation)
+orderRouter.route('/quotations/:quotationId').patch(verifyJWT, updateQuotationStatus)
+orderRouter.route('/stocklogs').get(verifyJWT, viewStockLog)
 
 
 
